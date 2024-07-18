d9dc9903ed57f307017a2753e1204bac472e0113
JosephWuAtPropertyMe <josephwu@propertyme.com>
2024-07-18 10:10:58 +1000

    first commit

---
+++ Program.cs
@@ -0,0 +1,29 @@
+﻿// See https://aka.ms/new-console-template for more information
+using Cassandra;
+
+Console.WriteLine("Hello, World!");
+
+var authProvider = new PlainTextAuthProvider("username", "password");
+var queryOptions = new QueryOptions()
+    .SetPageSize(1000);
+
+Cluster cluster = Cluster.Builder()
+    .AddContactPoint("192.168.1.100")
+    .WithPort(9042)
+    .WithQueryOptions(queryOptions)
+    .WithAuthProvider(authProvider)
+    .Build();
+
+var s = cluster.AllHosts();
+ISession session = cluster.Connect("demo_keyspace");
+
+
+
+var rows = session.Execute("SELECT * FROM messages");
+
+foreach (var row in rows)
+{
+    Console.WriteLine(row[0] + "," + row[1] + "," + row[2] + "," + row[3]);
+}
+
+Console.WriteLine("Break");
